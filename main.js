3bc37e1ac8f408661d749c0ab2c8eccc42132440
jupegarnica <jupegarnica@gmail.com>
2019-03-07 12:53:39 +0100

    complet task done

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 
-    let createTemplateHtmlString = text =>  `<div class="task">
+    let createTemplateHtmlString = text =>
+        `<div class="task">
             <div class="text">${text}</div>
-            <button>remove</button>
+            <button class="remove">remove</button>
+            <button class="complete">complete</button>
         </div>`
     let createNodeFromString = string => {
-        let divNode = document.createElement('div');
+        let divNode = document.createElement( 'div' );
         divNode.innerHTML = string;
         return divNode.firstChild;
     }
     let addRemoveListener = node => {
-        node.querySelector( 'button' ).addEventListener( 'click', event => {
+        node.querySelector( '.remove' ).addEventListener( 'click', event => {
             // event.target.parentNode.remove();
             node.remove();
         } )
     }
+    let addCompleteListener = node => {
+        node.querySelector('.complete').addEventListener('click', event => {
+            node.classList.toggle('completed')
+        })
+    }
     let inputNode = document.querySelector( 'header input' );
 
-    inputNode.addEventListener('keyup', function(event){
-        if (event.keyCode === 13) {
+    inputNode.addEventListener( 'keyup', function ( event ) {
+        if ( event.keyCode === 13 ) {
             //get value from input
             let newTaskText = event.target.value;
             // console.log(newTaskText);
 
             // creat html string from value text
-            let newTaskHtmlString = createTemplateHtmlString(newTaskText)
+            let newTaskHtmlString = createTemplateHtmlString( newTaskText )
             // console.log(newTaskHtmlString);
 
             // node creation from html string
-            let newTaskNode = createNodeFromString(newTaskHtmlString)
+            let newTaskNode = createNodeFromString( newTaskHtmlString )
             // console.log(newTaskNode)
 
-
             // node inject to DOM in main
-            document.querySelector('main').appendChild(newTaskNode)
+            document.querySelector( 'main' ).appendChild( newTaskNode )
 
             // clean value
             event.target.value = '';
 
-            addRemoveListener(newTaskNode);
+            addRemoveListener( newTaskNode );
+            addCompleteListener( newTaskNode );
         }
-    })
+    } )
 
 } )
